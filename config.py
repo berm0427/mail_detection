ec1c514cc87504284124fcb6cec01e59b1f3b319
berm0427 <berm0427@naver.com>
2026-01-09 19:16:23 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # config.py
-OPENAI_API_KEY = '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'
-ANTHROPIC_API_KEY = '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'
-PERPLEXITY_API_KEY = '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'
-GEMINI_API_KEY = 'AIzaSyDkzrccJZYEru1C7fM12Rtm5mNm3P_D2Vg'
-XAI_API_KEY = 'xai-cPcDH1Uam8nhK46IWiFg3OGPJt9UgJBKmDebXkOC9dnQv8RTRA97XkluPdQWyvjfAbLi1MfvFP8pzwWv'
+OPENAI_API_KEY = ''
+ANTHROPIC_API_KEY = ''
+PERPLEXITY_API_KEY = ''
+GEMINI_API_KEY = ''
+XAI_API_KEY = ''
 
 AI_ENABLED = True # True로 설정해야함
+
 AI_MODEL = 'multi-ai-grid'
